ab8d0651cdf25c22dae3fec5e55f499875ddb092
TSOlami <tijani.saheed@yahoo.com>
2023-07-25 18:53:16 +0100

    fixed all lint errors

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,12 +1,14 @@
+// Function to get the current year
 function getCurrentYear() {
-	const date = new Date();
-	return date.getFullYear();
-  }
-  
-  export default function getBudgetForCurrentYear(income, gdp, capita) {
-	return {
-	  [`income-${getCurrentYear()}`]: income,
-	  [`gdp-${getCurrentYear()}`]: gdp,
-	  [`capita-${getCurrentYear()}`]: capita,
-	};
-  }
+  const date = new Date();
+  return date.getFullYear();
+}
+
+// Function to get the budget for the current year
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
