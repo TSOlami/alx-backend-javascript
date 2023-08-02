04d7b0b79340bee33a3e8fbc45a94d1e1b943518
TSOlami <tijani.saheed@yahoo.com>
2023-08-02 18:12:14 +0100

    add the correct solutions to task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
 export default function getStudentsByLocation(list, city) {
-  // Filter out where the location is the same as the city 
+  // Filter out where the location is the same as the city
   return list.filter((obj) => obj.location === city);
 }
