322a896fa4b9d8c6cfcb73545f235755f2670adc
TSOlami <tijani.saheed@yahoo.com>
2023-09-19 10:34:10 +0100

    task 0 and 1 solution

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+// Define and export the displayMessage arrow function 
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+/* eslint-disable linebreak-style */
+// Display the welcome message using process.stdout.write
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
