72c99a791579cfb828873a436329848388704c30
TSOlami <tijani.saheed@yahoo.com>
2023-09-22 17:45:57 +0100

    fix task 4 solution

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,18 +1,32 @@
-/* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/expect-expect */
-const { describe, it } = require('mocha');
+/* eslint-disable jest/prefer-expect-assertions */
 const sinon = require('sinon');
 const assert = require('assert');
 const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  it('check that Utils.calculateNumber is stubbed', () => {
-    const spy = sinon.spy(console, 'log');
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  it('should call Utils.calculateNumber with type "SUM", a = 100, and b = 20', () => {
+    // Create a stub for Utils.calculateNumber
+    const stub = sinon.stub(Utils, 'calculateNumber');
+
+    // Configure the stub to return a specific value (e.g., 10)
+    stub.withArgs('SUM', 100, 20).returns(10);
+
+    // Create a spy for console.log
+    const consoleSpy = sinon.spy(console, 'log');
+
+    // Call the function we want to test
     sendPaymentRequestToApi(100, 20);
 
-    assert(spy.withArgs('The total is: 10').calledOnce);
-    assert(stub.withArgs('SUM', 100, 20).calledOnce);
+    // Verify that the stub was called with the expected arguments
+    assert(stub.calledOnceWithExactly('SUM', 100, 20));
+
+    // Verify that console.log was called with the correct message
+    assert(consoleSpy.calledOnceWithExactly('The total is: 10'));
+
+    // Restore both the stub and the console.log spy
+    stub.restore();
+    consoleSpy.restore();
   });
 });
