004c684a260d6457d27fdf047092e995e84ffbf4
TSOlami <tijani.saheed@yahoo.com>
2023-09-22 17:37:56 +0100

    fix task 4 solution

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,33 +1,18 @@
-/* eslint-disable jest/no-hooks */
-/* eslint-disable jest/expect-expect */
 /* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+const { describe, it } = require('mocha');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./4-payment'); // Update the file path if necessary
+const assert = require('assert');
+const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
-  let calculateNumberStub;
-  let consoleSpy;
-
-  beforeEach(() => {
-    calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10); // Stub the function to return 10
-    consoleSpy = sinon.spy(console, 'log');
-  });
-
-  afterEach(() => {
-    calculateNumberStub.restore(); // Restore the stub after each test
-    consoleSpy.restore(); // Restore the spy after each test
-  });
-
-  it('should call Utils.calculateNumber with type "SUM", a = 100, and b = 20', () => {
+  it('check that Utils.calculateNumber is stubbed', () => {
+    const spy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
     sendPaymentRequestToApi(100, 20);
 
-    sinon.assert.calledOnceWithExactly(calculateNumberStub, 'SUM', 100, 20);
-  });
-
-  it('should log the correct message "The total is: 10"', () => {
-    sendPaymentRequestToApi(100, 20);
-
-    sinon.assert.calledOnceWithExactly(consoleSpy, 'The total is: 10');
+    assert(spy.withArgs('The total is: 10').calledOnce);
+    assert(stub.withArgs('SUM', 100, 20).calledOnce);
   });
 });
