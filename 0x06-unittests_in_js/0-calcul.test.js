b45242c2979c38cf988e63707ca3037545ab6d9e
TSOlami <tijani.saheed@yahoo.com>
2023-09-22 12:20:51 +0100

    add test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,9 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
+  it('check for correct output', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
   it('check if a rounds down', () => {
     assert.strictEqual(calculateNumber(1.2, 3), 4);
   });
@@ -15,5 +18,11 @@ describe('calculateNumber', () => {
   });
   it('check if b rounds up', () => {
     assert.strictEqual(calculateNumber(1, 2.5), 4);
+  });
+  it('check if both rounds up', () => {
+    assert.strictEqual(calculateNumber(1.6, 1.5), 4);
+  });
+  it('check if both rounds down', () => {
+    assert.strictEqual(calculateNumber(2.1, 2.4), 4);
   });
 });
